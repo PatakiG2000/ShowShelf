feab6f6559cd92bf2a17a65c9e9c9c842255d0e7
PatakiGergo <pataki821@gmail.com>
2023-05-08 07:20:03 +0200

    Worked on homepage styling
    Worked on homepage styling

--- src/Layout/pages/Home.tsx
+++ src/Layout/pages/Home.tsx
@@ -4,8 +4,20 @@ export interface IHomeProps {}
 
 export default function Home(props: IHomeProps) {
   return (
-    <div>
-      <h1>This is the home</h1>
+    <div className="home">
+      <div className="home-leftside">
+        <div className="newsfeed-title">
+          <h3 className="newsfeed-title">Newsfeed</h3>
+        </div>
+        <div className="newsfeed-card"></div>
+        <div className="newsfeed-card"></div>
+        <div className="newsfeed-card"></div>
+      </div>
+      <div className="home-rightside">
+        <div className="main-recommendation">Upcoming</div>
+        <div className="continue-watching"></div>
+        <div className="Currently trending"></div>
+      </div>
     </div>
   );
 }
