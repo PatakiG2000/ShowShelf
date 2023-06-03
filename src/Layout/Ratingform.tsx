0f7b4159bf359bddedbd8c91ebfc3b806a70e92b
PatakiGergo <pataki821@gmail.com>
2023-06-03 10:15:47 +0200

    Refactored rating
    Refactored rating

--- src/Layout/Ratingform.tsx
+++ src/Layout/Ratingform.tsx
@@ -5,6 +5,7 @@ import { handleModal } from "../features/toplist/modalHandler";
 import { deleteFromWatchlist } from "../features/watchlist/watchlistSlice";
 import { deleteFromTracklist } from "../features/tracklist/tracklistSlice";
 import { MovieData } from "../interfaces/interfaces";
+import { useState } from "react";
 
 export interface IRatingFormProps {
   movieData: MovieData;
@@ -12,6 +13,21 @@ export interface IRatingFormProps {
 
 export default function Ratingform({ movieData }: IRatingFormProps) {
   const dispatch = useDispatch();
+
+  const [inputValues, setInputValues] = useState({
+    story: 50,
+    music: 50,
+    acting: 50,
+    ending: 50,
+  });
+
+  const handleInputChange = (event: any) => {
+    const { name, value } = event.target as HTMLInputElement;
+    setInputValues((prevInputValues) => ({
+      ...prevInputValues,
+      [name]: Number(value),
+    }));
+  };
 
   function handleClose() {
     dispatch(handleModal({ open: false, movieData: {} }));
@@ -24,21 +40,16 @@ export default function Ratingform({ movieData }: IRatingFormProps) {
 
   function formSubmit(e: React.FormEvent<HTMLFormElement>): void {
     e.preventDefault();
-    const target = e.target as HTMLFormElement;
-    const data = new FormData(target);
-    const formData: any = {};
     handleClose();
-    for (const [name, value] of data.entries()) {
-      formData[name] = value;
-    }
-    const { music, story, ending, acting } = formData;
-    formData.overallRating = Math.floor(
-      (parseInt(music) +
-        parseInt(story) +
-        parseInt(ending) +
-        parseInt(acting)) /
-        4
-    );
+    const { music, story, ending, acting } = inputValues;
+    const formData = {
+      music,
+      story,
+      ending,
+      acting,
+      overallRating: 0,
+    };
+    formData.overallRating = Math.floor((music + story + ending + acting) / 4);
 
     dispatch(
       addToToplist({
@@ -61,14 +72,42 @@ export default function Ratingform({ movieData }: IRatingFormProps) {
       <h1 className="rating-title">How did you enjoy "{movieData.title}" ?</h1>
       <div>
         <form id="form" className="form" action="" onSubmit={formSubmit}>
-          <label htmlFor="story">Story</label>
-          <input type="range" name="story" />
-          <label htmlFor="music">Music</label>
-          <input type="range" name="music" />
-          <label htmlFor="acting">Acting</label>
-          <input type="range" name="acting" />
-          <label htmlFor="ending">Ending</label>
-          <input type="range" name="ending" />
+          <label htmlFor="story">
+            Story <span>{inputValues.story}%</span>
+          </label>
+          <input
+            type="range"
+            name="story"
+            value={inputValues.story}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="music">
+            Music <span>{inputValues.music}%</span>
+          </label>
+          <input
+            type="range"
+            name="music"
+            value={inputValues.music}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="acting">
+            Acting <span>{inputValues.acting}%</span>
+          </label>
+          <input
+            type="range"
+            name="acting"
+            value={inputValues.acting}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="ending">
+            Ending <span>{inputValues.ending}%</span>
+          </label>
+          <input
+            type="range"
+            name="ending"
+            value={inputValues.ending}
+            onChange={handleInputChange}
+          />
 
           <label htmlFor="">What are your toughts?</label>
           <textarea
